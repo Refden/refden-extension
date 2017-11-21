b513a5eda41ddf18daf3ff9187229ab2e13c000f
Mario Perez <mapreal19@gmail.com>
2017-11-21 12:48:38 +0100

    Fetching refs

--- app/libs/services/informBackgroundTabShouldHavePageAction.js
+++ app/libs/services/informBackgroundTabShouldHavePageAction.js
@@ -3,5 +3,4 @@ import { FROM_CONTENT__SHOW_PAGE_ACTION } from '../messages';
 const informBackgroundTabShouldHavePageAction = () =>
   chrome.runtime.sendMessage(FROM_CONTENT__SHOW_PAGE_ACTION);
 
-
 export default informBackgroundTabShouldHavePageAction;
--- app/popup.js
+++ app/popup.js
@@ -1,7 +1,16 @@
+import axios from 'axios';
+
 import { FROM_POPUP__SHOW_REFERENCES } from './libs/messages';
 
-const setDOIs = dois =>
-  document.getElementById('dois').textContent = dois;
+const setDOIs = async (dois) => {
+  const response = await axios({
+    method: 'get',
+    url: `https://doi.org/${dois[0]}`,
+    headers: { 'Accept': 'application/json; charset=utf-8' },
+  });
+
+  document.getElementById('dois').textContent = response.data.title;
+}
 
 const showReferences = tabs =>
   chrome.tabs.sendMessage(tabs[0].id, FROM_POPUP__SHOW_REFERENCES, setDOIs);
