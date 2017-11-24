014e037fc42e3429861dd261088b5b9015a2bac0
Mario Perez <mapreal19@gmail.com>
2017-11-24 11:42:57 +0100

    Handles 404s

--- app/libs/api/referencesFetcher.js
+++ app/libs/api/referencesFetcher.js
@@ -6,13 +6,22 @@ export const BASE_URL = 'https://doi.org';
 const fecthReference = doi => axios({
   headers: { 'Accept': 'application/json; charset=utf-8' },
   url: `${BASE_URL}/${doi}`,
-});
+}).catch(error => error);
+
+const isValidResponse = _.flow(
+  _.get('status'),
+  _.lte(200) && _.gt(300),
+);
 
 const getTitle = _.get('data.title');
 
 const referencesFetcher = async (dois) => {
   const responses = await axios.all(dois.map(fecthReference));
-  return responses.map(getTitle);
+
+  return _.flow(
+    _.filter(isValidResponse),
+    _.map(getTitle),
+  )(responses);
 };
 
 export default referencesFetcher;
