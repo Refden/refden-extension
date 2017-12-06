a45a1047ece1140123261a445bbf3d839e243573
Mario Perez <mapreal19@gmail.com>
2017-12-06 11:27:46 +0100

    Populate lists

--- app/views/reference-form.js
+++ app/views/reference-form.js
@@ -1,8 +1,20 @@
-const populateForm = () => chrome.storage.sync.get('selectedReference', storageArea => {
+const populateLists = lists => {
+  const listSelect = document.getElementById('lists');
+
+  lists.forEach(list => {
+    const option = document.createElement('option');
+    option.textContent = list.name;
+    option.value = list.name;
+    listSelect.appendChild(option);
+  });
+};
+
+const populateForm = () => chrome.storage.sync.get(['selectedReference', 'lists'], storageArea => {
   const { doi, title } = storageArea.selectedReference;
 
   document.getElementById('doi').value = doi;
   document.getElementById('title').innerText = title;
+  populateLists(storageArea.lists);
 });
 
 document.addEventListener('DOMContentLoaded', populateForm);
