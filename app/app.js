d2cab00500c41a6342d1c5309ff197fd5cc7d7e4
Mario Perez <mapreal19@gmail.com>
2019-01-10 10:51:08 +0100

    Adds sentry

--- app/app.js
+++ app/app.js
@@ -1,6 +1,12 @@
+import { init } from '@sentry/browser';
+
 import informBackgroundTabShouldHavePageAction from './libs/services/informBackgroundTabShouldHavePageAction';
 import findReferences from './libs/listeners/findReferences';
 import setupBrowser from './libs/utils/setupBrowser';
+
+init({
+  dsn: 'https://ef8e3b5f472c409e9cd71ecc6c3611dc@sentry.io/1367650',
+});
 
 setupBrowser();
 
