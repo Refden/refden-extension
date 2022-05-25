5566eddaf2a79697311ae143206f23100b2de490
Fran CEACERO <franceacero@gmail.com>
2022-05-25 16:39:18 +0200

    allow action on text selection when right click

--- app/background.js
+++ app/background.js
@@ -4,3 +4,16 @@ import setupBrowser from './libs/utils/setupBrowser';
 setupBrowser();
 
 browser.runtime.onMessage.addListener(enablePageActionForTab);
+
+var searchTerapeak = function(word){
+    var query = word.selectionText;
+    // TODO open form with query as a title
+};
+
+browser.contextMenus.create({
+    id: "1",
+    title: "Save Highlight to Refden",
+    contexts:["selection"],
+});
+
+browser.contextMenus.onClicked.addListener(searchTerapeak);
